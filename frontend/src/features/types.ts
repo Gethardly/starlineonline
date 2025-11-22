59a5815be036b7ca0c75e7e5ea9dcf4e69a96d23
Bekzhan <bekzhan.kurmanaliev@gmail.com>
2025-11-23 04:41:12 +0600

    edit

--- frontend/src/features/types.ts
+++ frontend/src/features/types.ts
@@ -24,12 +24,26 @@ export interface Device {
     status: number;
 }
 
+export interface Position {
+    "id": number,
+    "x": number,
+    "y": number,
+    "name": string,
+    "address": string,
+    "contacts": string,
+    "description": string,
+    "note": string,
+    "createdAt": string,
+    "updatedAt": string
+}
+
 export const positionFormSchema = z.object({
+    positionId: z.string().optional(),
     device_id: z.string(),
     name: z.string().min(4, "Введите название места"),
     address: z.string().min(6, "Адрес обязателен"),
     contacts: z.string()
-        .min(17, "Введите полный номер")
+        .min(13, "Введите полный номер")
         .transform((val) => {
             return "+996" + val.replace(/[^\d]/g, "").slice(3);
         })
@@ -37,7 +51,7 @@ export const positionFormSchema = z.object({
             message: "Неверный номер телефона",
         }),
     description: z.string().min(5, "Введите описание"),
-    note: z.string().min(5, "Введите примечание"),
+    note: z.string().optional(),
     x: z.number(),
     y: z.number()
 });
